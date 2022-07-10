29398232c5014a458b55da2df2c76bce987f362e
Aarti Patel <royal.arati10@gmail.com>
2022-07-10 10:35:50 +0530

    Type of intent in android

---
+++ app/src/main/java/com/myappgensatsun/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.myappgensatsun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class HomeActivity extends AppCompatActivity {
+
+    TextView tvUserName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        tvUserName = findViewById(R.id.tv_username);
+
+        Intent i = getIntent();
+        String strUserName =  i.getStringExtra("KEY_USERNAME");
+        tvUserName.setText(strUserName);
+
+    }
+}
--- app/src/main/java/com/myappgensatsun/MainActivity.java
+++ app/src/main/java/com/myappgensatsun/MainActivity.java
@@ -2,13 +2,46 @@ package com.myappgensatsun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtUserName;
+    Button btnLogin;
+    ImageView imgLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtUserName = findViewById(R.id.edt_username);
+        btnLogin = findViewById(R.id.btn_login);
+        imgLogo = findViewById(R.id.img_logo);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             String strUserName = edtUserName.getText().toString();
+             if (strUserName.equals("")){
+                 Toast.makeText(MainActivity.this, "Please Enter Valid UserName", Toast.LENGTH_SHORT).show();
+             }else {
+                 imgLogo.setImageResource(R.drawable.dolphin_icon);
+                 Toast.makeText(MainActivity.this, "UserName is "+strUserName, Toast.LENGTH_SHORT).show();
+                // Explicit Intent
+                 Intent i = new Intent(MainActivity.this,HomeActivity.class);
+                 i.putExtra("KEY_USERNAME",strUserName);
+                 startActivity(i);
+
+             }
+            }
+        });
+
+
     }
 }
